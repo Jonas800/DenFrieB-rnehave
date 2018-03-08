20b27fa6e7bb95f06adb9c6cb203dbc0fe6ac982
“YOUR <YOUR>
2018-03-08 11:12:16 +0100

    Knap

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,14 +1,30 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 @Controller
 public class HomeController {
+    ArrayList<Child> child = new ArrayList<>();
 
     @GetMapping("/")
-    public String children(){
+    public String children() {
         return "boern";
     }
 
+    @GetMapping("/edit")
+    public String rediger(@RequestParam(value = "id", defaultValue = "1") int id, Model model) {
+        int børnID = 0;
+        for (int i = 0; i < child.size(); i++) {
+            if (child.get(i).getId() == id) {
+                børnID = i;
+            }
+        }
+        model.addAttribute("child", child.get(børnID));
+        return "edit";
+    }
 }
